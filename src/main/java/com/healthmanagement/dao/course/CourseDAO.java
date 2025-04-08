8d97bc3a8a975f94cb62beac6c2d7722ac505ff7
LAZY\YuChien <u841218@gmail.com>
2025-04-08 22:09:46 +0800

    新增course的CRUD

--- src/main/java/com/healthmanagement/dao/course/CourseDAO.java
+++ src/main/java/com/healthmanagement/dao/course/CourseDAO.java
@@ -1,8 +1,17 @@
 package com.healthmanagement.dao.course;
 
+import com.healthmanagement.model.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CourseDAO {
-    // 这个接口将由组员B实现
+public interface CourseDAO extends JpaRepository<Course, Integer> {
+	// 用課程名稱查詢
+    List<Course> findByNameContainingIgnoreCase(String name);
+    // 用教練ID查詢
+    List<Course> findByCoachId(Integer coachId);
+    // 用教練名稱查詢
+    List<Course> findByCoachNameContainingIgnoreCase(String coachName);
 }
