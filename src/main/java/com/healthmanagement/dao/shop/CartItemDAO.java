edeb11708dfec08eab13987507beda10c78b08c5
Ivan <Ivan@yahoo.com.tw>
2025-04-10 21:05:16 +0800

    測試

--- src/main/java/com/healthmanagement/dao/shop/CartItemDAO.java
+++ src/main/java/com/healthmanagement/dao/shop/CartItemDAO.java
@@ -5,9 +5,14 @@ import java.util.List;
 
 public interface CartItemDAO {
     CartItem save(CartItem cartItem);
+
     void delete(Integer id);
+
     CartItem findById(Integer id);
+
     List<CartItem> findByUserId(Integer userId);
+
     CartItem findByUserIdAndProductId(Integer userId, Integer productId);
+
     void deleteByUserId(Integer userId);
-} 
+}
